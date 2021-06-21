bcf4621904e69addf577a6159b03a06b2794ca8e
Kojo Idrissa <kojo.idrissa@gmail.com>
2021-06-21 14:26:39 -0500

    already tested

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,3 @@
 # How do I create two new users, each w/ a token, so I can test:
 # User1 can't see User2's status results and vice versa?
-# Do I NEED to test this, or do the Django views (and their tests) alread test this? 
+# Above proposed tests are covered by Django views (and their tests)
